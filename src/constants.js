8a79f45e029f85538705da9a1fd4cb18da9d50c7
Mikhail Karpov <mihanikk@gmail.com>
2019-03-10 16:40:42 +0300

    show Joined/Left checkboxes added

---
+++ src/constants.js
@@ -0,0 +1,31 @@
+export const axisFontColor = "#96A2AA";
+export const axisLinesColor = "#ECF0F3";
+export const joinedColor = "#3CC23F";
+export const leftColor = "#F34C44";
+export const chartFontSize = 0.03;
+
+export const months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec"
+];
+
+export const data = [
+  { date: "12-01-2018", joined: 134, left: 13 },
+  { date: "12-02-2018", joined: 45, left: 32 },
+  { date: "12-03-2018", joined: 12, left: 23 },
+  { date: "12-04-2018", joined: 150, left: 41 },
+  { date: "12-05-2018", joined: 230, left: 10 },
+  { date: "12-06-2018", joined: 134, left: 45 },
+  { date: "12-07-2018", joined: 87, left: 12 },
+  { date: "12-08-2018", joined: 129, left: 76 }
+];
--- src/index.js
+++ src/index.js
@@ -1,71 +1,129 @@
 import maxBy from "lodash/maxBy";
-import minBy from "lodash/minBy";
+import {
+  axisFontColor,
+  axisLinesColor,
+  joinedColor,
+  leftColor,
+  data,
+  months,
+  chartFontSize
+} from "./constants";
 
-let canvas;
-let ctx;
-let chart;
+let canvas,
+  ctx,
+  chart,
+  maxJoinedNum,
+  maxLeftNum,
+  maxNum,
+  maxValueY,
+  totalValues,
+  showJoined,
+  showLeft;
+let dpi = window.devicePixelRatio;
 
-const data = [
-  { date: "21-12-2018", joined: 134, left: 13 },
-  { date: "22-12-2018", joined: 45, left: 32 },
-  { date: "23-12-2018", joined: 12, left: 123 },
-  { date: "24-12-2018", joined: 110, left: 41 },
-  { date: "25-12-2018", joined: 230, left: 10 },
-  { date: "26-12-2018", joined: 134, left: 45 },
-  { date: "27-12-2018", joined: 87, left: 12 },
-  { date: "28-12-2018", joined: 12, left: 76 }
-];
+function fix_dpi() {
+  //create a style object that returns width and height
+  let style = {
+    height() {
+      return +getComputedStyle(canvas)
+        .getPropertyValue("height")
+        .slice(0, -2);
+    },
+    width() {
+      return +getComputedStyle(canvas)
+        .getPropertyValue("width")
+        .slice(0, -2);
+    }
+  };
+  //set the correct attributes for a crystal clear image!
+  canvas.setAttribute("width", style.width() * dpi);
+  canvas.setAttribute("height", style.height() * dpi);
+  ctx.scale(dpi, dpi);
+}
 
-const data2 = [
-  { date: "21-12-2018", joined: 134, left: 13 },
-  { date: "22-12-2018", joined: 45, left: 32 },
-  { date: "23-12-2018", joined: 12, left: 123 },
-  { date: "24-12-2018", joined: 110, left: 41 },
-  { date: "25-12-2018", joined: 337, left: 10 },
-  { date: "26-12-2018", joined: 134, left: 45 },
-  { date: "27-12-2018", joined: 87, left: 12 },
-  { date: "28-12-2018", joined: 12, left: 76 }
-];
+const checkboxes = document.querySelectorAll("input[type=checkbox]");
+checkboxes.forEach(function(el) {
+  el.addEventListener("change", onCheckboxChange);
+});
+showJoined = checkboxes[0].checked;
+showLeft = checkboxes[1].checked;
+
+function onCheckboxChange(e) {
+  if (e.target.name === "joined_checkbox") {
+    showJoined = e.target.checked;
+  } else {
+    showLeft = e.target.checked;
+  }
+
+  update();
+}
 
 function setup() {
   chart = {};
   createCanvas();
+  fix_dpi();
+
+  maxJoinedNum = showJoined ? maxBy(data, "joined").joined : 0;
+  maxLeftNum = showLeft ? maxBy(data, "left").left : 0;
+  maxNum = Math.max(maxJoinedNum, maxLeftNum);
+
+  maxValueY = getYAxisMaxValue(maxNum);
+  totalValues = data.length;
 
   drawGridLines();
-  drawChart();
+  drawDatesLegend(data);
+  showJoined && drawChart(data, joinedColor);
+  showLeft && drawChart(data, joinedColor);
+  drawAxis(data);
+}
+
+function update() {
+  maxJoinedNum = showJoined ? maxBy(data, "joined").joined : 0;
+  maxLeftNum = showLeft ? maxBy(data, "left").left : 0;
+  maxNum = Math.max(maxJoinedNum, maxLeftNum);
+  if (maxNum === 0) return;
+  maxValueY = getYAxisMaxValue(maxNum);
+  totalValues = data.length;
+
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawGridLines();
+  drawDatesLegend(data);
+  showJoined && drawChart(data, "joined", joinedColor);
+  showLeft && drawChart(data, "left", leftColor);
   drawAxis(data);
 }
 
 function createCanvas() {
   canvas = document.createElement("canvas");
+  canvas.id = "main_chart";
   document.getElementById("app").appendChild(canvas);
   canvas.style = `
-            width: 95%;
+            width: 100%;
+            height: 50%;
         `;
   ctx = canvas.getContext("2d");
-  ctx.translate(0.5, 0.5);
   resize();
 }
 
 function resize() {
   const { innerWidth, innerHeight } = window;
-
+  console.log(innerWidth + ";" + innerHeight + ";" + dpi);
   canvas.width = innerWidth;
-  canvas.height = innerHeight;
+  canvas.height = innerHeight * dpi;
   chart.minW = 0.05 * innerWidth;
   chart.maxW = 0.95 * innerWidth;
   chart.minH = 0.05 * innerHeight;
   chart.maxH = 0.95 * innerHeight;
-  chart.width = innerWidth * 0.9;
-  chart.height = innerHeight * 0.9;
+  chart.width = innerWidth;
+  chart.height = innerHeight;
+  fix_dpi();
+  requestAnimationFrame(update);
 }
 
 function getYAxisMaxValue(max) {
   const divider = Math.max(10, 10 ** (max.toString().length - 1));
   const closestMax = Math.ceil(max / divider) * divider;
   const maxValue = Math.max(closestMax, Math.round(max / divider) * divider);
-
-  // console.log(max + " = divider:" + divider + ", " + maxValue);
   return maxValue;
 }
 
@@ -74,19 +132,27 @@ function drawAxis() {
   ctx.beginPath();
   ctx.moveTo(chart.minW, chart.maxH);
   ctx.lineTo(chart.maxW, chart.maxH);
-  ctx.strokeStyle = "#ECF0F3";
+  ctx.strokeStyle = axisLinesColor;
   ctx.stroke();
 }
 
-function drawLegend() {}
+function drawDatesLegend(data) {
+  ctx.font = `${chart.height * chartFontSize}px -apple-system`;
+  ctx.fillStyle = axisFontColor;
+
+  data.map((value, i) => {
+    const pos = chart.minW + (i * chart.width) / totalValues;
+    console.log(value.date);
+    const date = new Date(value.date);
+    const label = months[date.getMonth()] + " " + date.getDate();
+    ctx.fillText(label, pos, chart.maxH + chart.height * 0.03);
+  });
+}
 
 function drawGridLines() {
-  const maxNum = maxBy(data, "joined").joined;
-  const maxValueY = getYAxisMaxValue(maxNum);
-  console.log(maxNum);
-  ctx.font = "20px";
-  ctx.strokeStyle = "#ECF0F3";
-  ctx.fillStyle = "#96A2AA";
+  ctx.font = `${chart.height * chartFontSize}px -apple-system`;
+  ctx.strokeStyle = axisLinesColor;
+  ctx.fillStyle = axisFontColor;
 
   for (let i = 1; i <= 5; i++) {
     const height = chart.maxH - (i * chart.height) / 5;
@@ -101,16 +167,21 @@ function drawGridLines() {
   ctx.fillText(0, chart.minW, chart.maxH - chart.height * 0.02);
 }
 
-function drawChart() {
+function drawChart(data, type, color) {
   ctx.beginPath();
   ctx.moveTo(chart.minW, chart.maxH);
-  ctx.lineTo(canvas.width * 0.2, canvas.height * 0.3);
-  ctx.lineTo(canvas.width * 0.3, canvas.height * 0.6);
+  data.map((value, i) => {
+    const y = chart.height - ((value[type] / maxValueY) * chart.height + chart.minH);
+    const x = chart.minW + (i / totalValues) * chart.width;
+    console.log(value[type] + "|" + x + "|" + y);
+    i === 0 ? ctx.moveTo(x, y) : ctx.lineTo(x, y);
+  });
+
   ctx.lineCap = "round";
-  ctx.strokeStyle = "#3CC23F";
-  ctx.lineWidth = 4;
+  ctx.strokeStyle = color;
+  ctx.lineWidth = 3;
   ctx.stroke();
 }
 
 window.addEventListener("load", setup);
-// window.addEventListener("resize", resize);
+window.addEventListener("resize", resize);
